33479c2fd439c9254a427f63783f7450a0c39c39
Anthony Grimes <agrimes@charter.net>
2017-09-19 18:59:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,7 +11,7 @@ function init() {
          index ++;
 
          if (index === code.length) {
-             alert("You ");
+             alert("You Have CRACKED The CODE!");
 
              index = 0;
          }
