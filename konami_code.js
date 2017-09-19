882f1d8c6646d44386042f027c288af3c2b7a635
Anthony Grimes <agrimes@charter.net>
2017-09-19 19:37:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0;
-
 function init() {
   // Write your JavaScript code inside the init() function
+  var index = 0;
+
   document.addEventListener("keydown", function(e) {
      const key = parseInt(e.detail || e.which);
 
@@ -18,7 +18,7 @@ function init() {
      } else {
          index = 0;
      }
- }, true);
+ }, false);
 }
 
 init();
