17d7fd15482c93e7611e5f4e91795560199c9601
Anthony Grimes <agrimes@charter.net>
2017-09-19 19:38:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ function init() {
      if (key === code[index]) {
          index ++;
 
-         if (index === code.length) {
+         if (index === code.length - 1) {
              window.alert("YOU DID IT!");
 
              index = 0;
