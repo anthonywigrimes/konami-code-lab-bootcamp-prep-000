437a4ad3322382ec33ddacde0c45b8eb8d7748f1
Anthony Grimes <agrimes@charter.net>
2017-09-19 18:47:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
   document.addEventListener("keydown", function(e) {
-     alert("Key: " + e.detail);
+     alert("Key: " + e.which);
   });
 }
 
